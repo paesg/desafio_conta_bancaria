f25c59a276ce49670081786ef983bc7f5d2eb37a
Gabriel Paes <gabrielpaes99@gmail.com>
2024-09-25 20:30:44 -0300

    feat: :sparkles: Adicionado operação de extrato
    Adicionado operação de extrato e corrigido o fluxo de saque e depósito

--- desafio.py
+++ desafio.py
@@ -7,51 +7,66 @@ menu = """
 
 => """
 
-saldo = 1000
+saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-def deposito(saldo):
-    valor = float(input("Insira o valor para depositar: "))
 
-    if valor > 0: 
+def deposito(saldo, extrato):
+    valor = float(input("Insira o valor para depositar: "))
+    if valor <= 0:
+        print("Valor inválido.")
+
+    elif valor > 0:
         saldo += valor
-        print(f"Seu novo saldo é {saldo}")
-        return saldo
-    
-def saque(saldo, numero_saques):
-    
+        print(f"Seu novo saldo é R$ {saldo:.2f}")
+        extrato += f"Depósito no valor de R$ {valor:.2f} \n"
+    return saldo, extrato
+
+
+def saque(saldo, numero_saques, extrato):
+
     if numero_saques >= 3:
         print("Limite de saque diário atingido")
         return saldo, numero_saques
-        
+
     valor = float(input("Insira o valor para sacar: "))
-    if valor > 0 and valor <= saldo:
+
+    if valor > 500.01:
+        print("Valor máximo para saque é de R$ 500.00")
+
+    elif valor > saldo:
+        print("Saldo insuficiente, Operação não concluida.")
+
+    elif valor <= 0:
+        print("Valor inválido.")
+
+    elif valor > 0 and valor <= saldo and valor <= 500.00:
         numero_saques += 1
         saldo -= valor
-        print(f"Seu novo saldo é {saldo}")
-    return saldo, numero_saques
+        print(f"Seu novo saldo é R$ {saldo:.2f}")
+        extrato += f"Saque no valor de R$ {valor:.2f} \n"
+    return saldo, numero_saques, extrato
+
 
 while True:
-    opcao = input (menu)
-    
+    opcao = input(menu)
+
     if opcao == "d":
         print("Depósito")
-        saldo = deposito(saldo)
-    
+        saldo, extrato = deposito(saldo, extrato)
+
     elif opcao == "s":
         print("Saque")
-        saldo, numero_saques = saque(saldo, numero_saques)
+        saldo, numero_saques, extrato = saque(saldo, numero_saques, extrato)
     elif opcao == "e":
-        print(f"Extrato {saldo}")
-        
+        print("\n-------------Extrato-------------")
+        print("Não foram realizadas transações." if not extrato else extrato)
+        print(f"Saldo atual R$: {saldo:.2f}")
     elif opcao == "q":
         break
-    
-    
+
     else:
         print("Operação inválida, por favor selecione  novamente a operação desejada.")
-        
-
